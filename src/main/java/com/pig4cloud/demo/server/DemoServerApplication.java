d0f2b58caee5c6fc3d5bbef8a5eeba802a8eaaed
lengleng <wangiegie@gmail.com>
2020-12-21 20:33:26 +0800

    :memo: Writing docs. readme

--- src/main/java/com/pig4cloud/demo/server/DemoServerApplication.java
+++ src/main/java/com/pig4cloud/demo/server/DemoServerApplication.java
@@ -2,9 +2,17 @@ package com.pig4cloud.demo.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class DemoServerApplication {
+
+	@GetMapping
+	public String index(){
+		return  "最小镜像挑战赛 </br> © 2020 pig4cloud.com" ;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoServerApplication.class, args);
